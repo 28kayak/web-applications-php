75cc08afa0720fbc7fa077dc9ab7a5ee7a5da70b
kayak28 <0246kaya>
2021-09-04 16:57:38 +0900

    edit index.php

---
+++ week5/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title>Kaya Ota</title>
+</header>
+<body>
+<h1>Kaya Ota</h1>
+<?php 
+$name = "
+**    ** 
+**   **  
+**  **   
+*****    
+**  **   
+**   **  
+**    ** 
+";
+$my_hash = hash('sha256', 'Kaya Ota');
+?>
+<pre>
+    <?php
+     echo $name 
+    ?>
+</pre>
+
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
