19a829bd6f5e76337aec4b05363a4100ee2df4be
kayak28 <0246kaya>
2021-09-04 17:07:47 +0900

    add display my_hash and link to other php files

--- week5/index.php
+++ week5/index.php
@@ -19,9 +19,12 @@ $my_hash = hash('sha256', 'Kaya Ota');
 ?>
 <pre>
     <?php
-     echo $name 
+     echo $name;
+     echo $my_hash;
     ?>
 </pre>
+<a href="week5/fail.php"> Clieck here to jump fail.php</a>
+<a href="week5/check.php"> Clieck here to jump check.php</a>
 
 
 <?php
